4dbc6d34409a91b2d5b11de3b48fed7ac27ee589
lirucy <rick_hertel77@msn.com>
2021-09-26 16:17:25 -0600

    add responsive design to Privacy, Contact and Terms Components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -74,67 +74,69 @@ class Form extends Component {
 
   render() {
     return (
-      <div className="home-body">
+      <div>
         <div className="divider">
           <h2 className="divider-text">theHODL</h2>
         </div>
-        <div className="instruct-div">
-          <p className="instructions">
-            Welcome to theHODL! An interactive, social community where you can
-            comment on, rate and post your favorite cryptocurrencies. To get
-            started, enter the desired cryptocurrency name along with remaining
-            fields below and submit.
-          </p>
-        </div>
-        <main>
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-div">
-              <label htmlFor="crypto">Crypto: </label>
-              <div className="typeahead-container">
+        <div className="home-body">
+          <div className="instruct-div">
+            <p className="instructions">
+              Welcome to theHODL! An interactive, social community where you can
+              comment on, rate and post your favorite cryptocurrencies. To get
+              started, enter the desired cryptocurrency name along with
+              remaining fields below and submit.
+            </p>
+          </div>
+          <main>
+            <form onSubmit={this.handleSubmit}>
+              <div className="form-div">
+                <label htmlFor="crypto">Crypto: </label>
+                <div className="typeahead-container">
+                  <input
+                    type="text"
+                    value={this.state.crypto}
+                    placeholder="enter crypto here"
+                    required
+                    autoFocus
+                    onChange={(e) => this.setState({ crypto: e.target.value })}
+                    onClick={() => this.closeList()}
+                  />
+                  {this.getMatchingCryptoNames()}
+                </div>
+                <label htmlFor="userName"> Name: </label>
                 <input
                   type="text"
-                  value={this.state.crypto}
-                  placeholder="enter crypto here"
+                  value={this.state.userName}
+                  placeholder="enter your name here"
                   required
-                  autoFocus
-                  onChange={(e) => this.setState({ crypto: e.target.value })}
-                  onClick={() => this.closeList()}
+                  onChange={(e) => this.setState({ userName: e.target.value })}
                 />
-                {this.getMatchingCryptoNames()}
+                <label htmlFor="thoughts"> Comment: </label>
+                <input
+                  type="text"
+                  value={this.state.comment}
+                  placeholder="enter comments here"
+                  required
+                  onChange={(e) => this.setState({ comment: e.target.value })}
+                />
+                <label htmlFor="rating">Rating: {this.state.rating}/10</label>
+                <input
+                  id="rating"
+                  type="range"
+                  required
+                  step={1}
+                  min={1}
+                  max={10}
+                  value={this.state.rating}
+                  onChange={(e) =>
+                    this.setState({ rating: e.target.valueAsNumber })
+                  }
+                />
+                <button type="submit" className="submit-button">Submit</button>
               </div>
-              <label htmlFor="userName"> Name: </label>
-              <input
-                type="text"
-                value={this.state.userName}
-                placeholder="enter your name here"
-                required
-                onChange={(e) => this.setState({ userName: e.target.value })}
-              />
-              <label htmlFor="thoughts"> Comment: </label>
-              <input
-                type="text"
-                value={this.state.comment}
-                placeholder="enter comments here"
-                required
-                onChange={(e) => this.setState({ comment: e.target.value })}
-              />
-              <label htmlFor="rating">Rating: {this.state.rating}/10</label>
-              <input
-                id="rating"
-                type="range"
-                required
-                step={1}
-                min={1}
-                max={10}
-                value={this.state.rating}
-                onChange={(e) =>
-                  this.setState({ rating: e.target.valueAsNumber })
-                }
-              />
-              <button type="submit">Post</button>
-            </div>
-          </form>
-        </main>
+            </form>
+          </main>
+        </div>
       </div>
     );
   }
