9d931f0876e927bebc27345a92143e9da8fbb30d
lirucy <rick_hertel77@msn.com>
2021-09-27 17:20:58 -0600

    resize logo and nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,16 +7,17 @@ class Nav extends Component {
     return (
       <nav className="nav">
         <Link to="/" >
-          <div className="the-hodl"><img src="https://iili.io/RiwpY7.jpg" alt="home" /></div>
+          {/* <div className="the-hodl"><img src="https://iili.io/RiwpY7.jpg" alt="home" /></div> */}
+          <div className="the-hodl"><img src="https://iili.io/RtyQrg.jpg" alt="home" /></div>
           <div className="title">
             <h4>HODL Home</h4>
           </div>
         </Link>
         <Link to="/history" target="_blank" className="history">
-          <div className="nav-history-link"><h3>History</h3></div>
+          <div className="nav-history-link"><h3 className="history-text">History</h3></div>
         </Link>
         <Link to="/forum" target="_blank" className="forum">
-          <div className="nav-forum-link"><h3>Forum</h3></div>
+          <div className="nav-forum-link"><h3 className="forum-text">Forum</h3></div>
         </Link>
       </nav>
     );
